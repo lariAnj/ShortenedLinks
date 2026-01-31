5c53367b94d303baa4c081bdd30d0a1e30da3fef
lariAnj <alarisa2005@gmail.com>
2026-01-31 16:44:12 +0300

    Init commit

---
+++ src/main/java/un/links/shortenedlinks/ShortenedLinksApplication.java
@@ -0,0 +1,13 @@
+package un.links.shortenedlinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenedLinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenedLinksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/un/links/shortenedlinks/ShortenedLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package un.links.shortenedlinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenedLinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
